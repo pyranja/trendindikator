8c95417b838f0ebebb9267baa936773d7723c4b5
Chris Borckholder <borckholder@gmail.com>
2013-01-30 21:59:24 +0100

    resource clean up in main

--- trendindikator/main.py
+++ trendindikator/main.py
@@ -10,11 +10,14 @@ import test.mock_view
 from core.index import CsvRepository
 
 if __name__ == '__main__':
-    repo = CsvRepository(os.getcwd())
-    view = test.mock_view.MockView()
-    graph_view = test.mock_view.GraphView()
-    graph_presenter = gui.presenter.GraphPresenter(graph_view, repo)
-    main_presenter = gui.presenter.SettingsPresenter(view, graph_presenter, repo)
-    main_presenter.validate_view()
-    main_presenter.invoke_pipe()
-    main_presenter.update_index()
+    try:
+        repo = CsvRepository(os.getcwd())
+        view = test.mock_view.MockView()
+        graph_view = test.mock_view.GraphView()
+        graph_presenter = gui.presenter.GraphPresenter(graph_view, repo)
+        main_presenter = gui.presenter.SettingsPresenter(view, graph_presenter, repo)
+        main_presenter.validate_view()
+        main_presenter.invoke_pipe()
+        main_presenter.update_index()
+    finally:
+        repo.clear()    # clean up!
